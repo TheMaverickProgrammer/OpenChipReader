f3252187e3202c8c7ec3272fd66b5f5c91e81d5e
TheMaverickProgrammer <bmpeppers@gmail.com>
2020-01-12 11:50:40 -0600

    first commit

---
+++ chip_reader.ino
@@ -0,0 +1,39 @@
+void setup() {
+  // Open serial connection.
+  Serial.begin(9600);
+
+  for(int i = 11; i > 1; i--){
+    //pinMode(i, INPUT);
+    pinMode(i, INPUT_PULLUP); // Enable internal pull-up resistor on pin
+  }
+
+  // I made 0 power because I have the chip reader upside down on the board
+  //pinMode(0, OUTPUT); // power
+}
+
+void loop() {
+  int val = 0;
+  String binary = String('1');
+
+  for(int i = 11; i > 1; i--) {
+    val = digitalRead(i);   // read the input pin
+
+    if(val == HIGH) {
+      val = LOW;
+    }else{
+      val = HIGH;
+    }
+    binary += String(val);
+  }
+
+  auto output = String();
+
+  for(int i = 0; i < binary.length()-2; i++) {
+    output += binary[i];
+  }
+
+  output += String('1');
+  
+  Serial.println(output);
+  delay(1000); // ms
+}
